b841bfdd123c5e498499a936f9a3583067d5eef5
MackInTech <jerrykmack803@yahoo.com>
2022-11-30 22:52:49 -0500

    november updates

--- js/script.js
+++ js/script.js
@@ -13,65 +13,15 @@ $(document).ready(function () {
 
   var typed = new Typed(".typed", {
     strings: [
-      "Software Engineering, B.S.",
-      "Marine Corps Avionics Tech",
       "Software Engineer",
-      "Aeronautics, A.S.",
+      "Digital Creator",
+      "Web Designer",
+      "Marine Corps Veteran",
     ],
-    typeSpeed: 70,
+    typeSpeed: 45,
     loop: true,
-    startDelay: 1000,
+    startDelay: 300,
     showCursor: false,
-  });
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    items: 4,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      480: {
-        items: 2,
-      },
-      768: {
-        items: 3,
-      },
-      938: {
-        items: 4,
-      },
-    },
-  });
-
-  var skillsTopOffset = $(".skillsSection").offset().top;
-  var statsTopOffset = $(".statsSection").offset().top;
-  var countUpFinished = false;
-
-  $(window).scroll(function () {
-    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-      $(".chart").easyPieChart({
-        easing: "easeInOut",
-        barColor: "rgb(0, 0, 0)",
-        trackColor: false,
-        scaleColor: false,
-        lineWidth: 4,
-        size: 152,
-        onStep: function (from, to, percent) {
-          $(this.el).find(".percent").text(Math.round(percent));
-        },
-      });
-    }
-    if (
-      !countUpFinished &&
-      window.pageYOffset > statsTopOffset - $(window).height() + 200
-    ) {
-      $(".counter").each(function () {
-        var element = $(this);
-        var endVal = parseInt(element.text());
-
-        element.countup(endVal);
-      });
-      countUpFinished = true;
-    }
   });
 
   $("#navigation li a").click(function (e) {
